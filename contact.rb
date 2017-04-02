dd92d8b5f245d296e05542a199bf2ad84d082423
Mark <markjconlon@gmail.com>
2017-04-02 16:45:18 -0400

    finished delete all full name and delete

--- contact.rb
+++ contact.rb
@@ -94,17 +94,22 @@ class Contact
 
   # This method should delete all of the contacts
   def self.delete_all
-
+    @@contacts = []
   end
 
   def full_name
-
+    "#{@first_name} #{last_name}"
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
-  def delete
 
+  #This method deletes every block which evaluates to true so have to use the unique_id
+  def delete
+    remove_id = self.id
+    @@contacts.delete_if do |contact|
+      contact.id == remove_id
+    end
   end
 
   # Feel free to add other methods here, if you need them.
